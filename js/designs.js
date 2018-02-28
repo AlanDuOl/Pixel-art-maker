0aed704a91ec3975432b30c5ce32dd816e280c8a
AlanDuOl <alanduarte90@gmail.com>
2018-02-28 18:12:39 -0300

    Add files via upload
    update files and add jQuery library

--- js/designs.js
+++ js/designs.js
@@ -1,1 +1,11 @@
-//designs
+// designs for Pixel Art Maker
+
+let width, height, color, submitBtn;
+submitBtn = $("#submit-btn");
+height = $("#height-value");
+width = $("#width-value");
+
+submitBtn.on('click', function(){
+      let val = width.val();
+      alert(val);
+  });
