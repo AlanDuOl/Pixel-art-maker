5ee254116468bec5e191632d55aee6a63e9b3dbf
AlanDuOl <alanduarte90@gmail.com>
2018-03-01 00:50:16 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,11 +1,25 @@
 // designs for Pixel Art Maker
 
-let width, height, color, submitBtn;
+let grid_width, grid_height, color, submitBtn, table;
 submitBtn = $("#submit-btn");
-height = $("#height-value");
-width = $("#width-value");
+grid_width = $("#width-value");
+grid_height = $("#height-value");
+color = $("#picked-color");
+table = $("table");
+let width = grid_width.val();
+let height = grid_height.val();
 
-submitBtn.on('click', function(){
-      let val = width.val();
-      alert(val);
-  });
+submitBtn.click($(function(){
+      width = grid_width.val();
+      height = grid_height.val();
+
+      $("footer").append("<p>"+width+"</p>");
+
+      for(let row = 0; row < width; row++){
+        let newRow = table.append("<tr>");
+        for(let col = 0; col < height; col++){
+          newRow.append("<td>");
+        }
+        table.append("</tr>");
+      }
+  }));
