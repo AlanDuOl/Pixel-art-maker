1fe0c90e2d555c8c846da21f87de5dafdedc4daf
AlanDuOl <alanduarte90@gmail.com>
2018-03-01 17:52:23 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,25 +1,46 @@
 // designs for Pixel Art Maker
 
-let grid_width, grid_height, color, submitBtn, table;
-submitBtn = $("#submit-btn");
+let grid_width, grid_height, color, submitForm, table;
+submitForm = $("#updateForm");
 grid_width = $("#width-value");
 grid_height = $("#height-value");
 color = $("#picked-color");
 table = $("table");
-let width = grid_width.val();
-let height = grid_height.val();
 
-submitBtn.click($(function(){
+// submitBtn.click($(function(){
+//     var width, height;
+//
+//     $.ajax({ url: "js/designs.js",
+//         cache: false,
+//         contentType: "application/json; charset=utf-8",
+//         dataType: "html",
+//         type: "post",
+//         data: { "width": width },
+//         success: function (result) {
+//             alert(result);
+//         },
+//         error: function (xhr, ajaxOptions, thrownError) {
+//             alert("Error: " + thrownError);
+//         }
+//     });
+let width, height;
+$(function makeGrid(){
       width = grid_width.val();
       height = grid_height.val();
+      submitForm.submit(function(){
+          width = grid_width.val();
+          height = grid_height.val();
 
-      $("footer").append("<p>"+width+"</p>");
+      });
 
       for(let row = 0; row < width; row++){
-        let newRow = table.append("<tr>");
+        let newRow = $('table').append("<tr>");
         for(let col = 0; col < height; col++){
           newRow.append("<td>");
         }
         table.append("</tr>");
       }
-  }));
+      $('footer').append("<p>" + width + "</p>");
+});
+
+// document.getElementById("mySubmit").value = "newSubmitButtonValue";
