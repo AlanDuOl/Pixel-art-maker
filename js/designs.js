93ebe12f8a963d8592feabf03f6dd55587453d2d
AlanDuOl <alanduarte90@gmail.com>
2018-03-03 12:05:33 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,46 +1,44 @@
 // designs for Pixel Art Maker
 
 let grid_width, grid_height, color, submitForm, table;
-submitForm = $("#updateForm");
+submitForm = $("#submitForm");
 grid_width = $("#width-value");
 grid_height = $("#height-value");
 color = $("#picked-color");
 table = $("table");
 
-// submitBtn.click($(function(){
-//     var width, height;
-//
-//     $.ajax({ url: "js/designs.js",
-//         cache: false,
-//         contentType: "application/json; charset=utf-8",
-//         dataType: "html",
-//         type: "post",
-//         data: { "width": width },
-//         success: function (result) {
-//             alert(result);
-//         },
-//         error: function (xhr, ajaxOptions, thrownError) {
-//             alert("Error: " + thrownError);
-//         }
-//     });
-let width, height;
+//TRY to set property value with document element to make it a loop
+let grid = {"width":10,"height":10};
+
+onSubmit();
+
+function onSubmit(){
+    // grid_width.val(16);
+    // grid_height.val(15);
+    grid['width'] = grid_width.val();
+    grid['height'] = grid_height.val();
+    color.val('rgb(255, 12,25)');
+    // $.post("index.html", function(data){
+    //     let val = data.serialize();
+    // alert(grid['width']);
+    // })
+}
+
 $(function makeGrid(){
-      width = grid_width.val();
-      height = grid_height.val();
-      submitForm.submit(function(){
-          width = grid_width.val();
-          height = grid_height.val();
-
-      });
-
-      for(let row = 0; row < width; row++){
-        let newRow = $('table').append("<tr>");
-        for(let col = 0; col < height; col++){
-          newRow.append("<td>");
-        }
-        table.append("</tr>");
+    width = grid['width'];
+    height = grid['height'];
+    // //width = submitForm.submit().$("#width-value").val();
+    // submitForm.submit($(function(){
+    //     width = grid_width.val();
+    //     height = grid_height.val();
+        //document.cookie = `width=;${grid_width.val()}`;
+        //$('footer').append("<p>" + val + "</p>");
+    for(let row = 0; row < height; row++){
+      let newRow = $('table').append("<tr>");
+      for(let col = 0; col < width; col++){
+        newRow.append("<td>");
       }
-      $('footer').append("<p>" + width + "</p>");
+      table.append("</tr>");
+    }
+    $('footer').append("<p>" + width + "</p>");
 });
-
-// document.getElementById("mySubmit").value = "newSubmitButtonValue";
