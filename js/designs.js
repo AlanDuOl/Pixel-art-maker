5e874da8ff331a3598f2d3eb5aec113e76aa47e7
AlanDuOl <alanduarte90@gmail.com>
2018-03-05 00:24:16 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,44 +1,55 @@
-// designs for Pixel Art Maker
-
-let grid_width, grid_height, color, submitForm, table;
-submitForm = $("#submitForm");
-grid_width = $("#width-value");
-grid_height = $("#height-value");
-color = $("#picked-color");
-table = $("table");
+// Select color input
+// Select size input
+// When size is submitted by the user, call makeGrid()
 
 //TRY to set property value with document element to make it a loop
-let grid = {"width":10,"height":10};
 
-onSubmit();
+let grid_width, grid_height, color, submitForm, table, newRow;
+submitForm = document.getElementById("sizePicker");
+grid_width = document.getElementById("inputWidth");
+grid_height = document.getElementById("inputHeight");
+color = document.getElementById("colorPicker");
+table = document.getElementById("pixelCanvas");
+// td = document.createElement("td");
+// trO = document.createElement("tr");
+// trC = document.createElement("/tr");
 
-function onSubmit(){
-    // grid_width.val(16);
-    // grid_height.val(15);
-    grid['width'] = grid_width.val();
-    grid['height'] = grid_height.val();
-    color.val('rgb(255, 12,25)');
-    // $.post("index.html", function(data){
-    //     let val = data.serialize();
-    // alert(grid['width']);
-    // })
+// submitForm.onsubmit = function() {
+//     width = getParameterByName('width');
+//     height = getParameterByName('height');
+//     //alert(width);
+// };
+makeGrid();
+
+function makeGrid() {
+
+    width = getParameterByName('width');
+    height = getParameterByName('height');
+    //alert(width+" "+height);
+    for(let row = 0; row < height; row++){
+      let trO = document.createElement("tr");
+      table.appendChild(trO);
+      for(let col = 0; col < width; col++){
+        let td = document.createElement("td");
+        trO.appendChild(td);
+      }
+      //table.appendChild("</tr>");
+    }
 }
 
-$(function makeGrid(){
-    width = grid['width'];
-    height = grid['height'];
-    // //width = submitForm.submit().$("#width-value").val();
-    // submitForm.submit($(function(){
-    //     width = grid_width.val();
-    //     height = grid_height.val();
-        //document.cookie = `width=;${grid_width.val()}`;
-        //$('footer').append("<p>" + val + "</p>");
-    for(let row = 0; row < height; row++){
-      let newRow = $('table').append("<tr>");
-      for(let col = 0; col < width; col++){
-        newRow.append("<td>");
-      }
-      table.append("</tr>");
-    }
-    $('footer').append("<p>" + width + "</p>");
-});
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// var foo = getParameterByName('foo');
+// var bar = getParameterByName('bar');
+// var baz = getParameterByName('baz');
+// var qux = getParameterByName('qux');
+
+//submitForm.addEventListener("submit", getGridSize);
