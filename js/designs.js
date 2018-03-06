d41c670a76fc35318364fe8cb3e9216e1638792c
AlanDuOl <alanduarte90@gmail.com>
2018-03-05 23:20:46 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -1,4 +1,4 @@
-//declare and set the global variables
+//Declare and set the global variables
 let tableDataIds = [];
 let grid_width, grid_height, color, table;
 grid_width = document.getElementById("inputWidth");
@@ -6,37 +6,39 @@ grid_height = document.getElementById("inputHeight");
 color = document.getElementById("colorPicker");
 table = document.getElementById("pixelCanvas");
 
+//Call makeGrid
 makeGrid();
 
+//Create the grid with the width and height set
 function makeGrid() {
-    //grab the input value for grid width and height
-    width = getParameterByName('width');
-    height = getParameterByName('height');
-    //check if initial value is set
+    //Grab the input value for grid width and height
+    let width = getParameterByName('width');
+    let height = getParameterByName('height');
+    //Check if initial value is set. If not set it to default 10x10
     if (width === null || height === null) {
         width = 10;
         height = 10;
     }
 
-    //set the input field value to the corresponding grid width and height
+    //Set the input field value to the corresponding grid width and height
     grid_width.value = width;
     grid_height.value = height;
 
-    //draw the canvas looping through the lines(height) and columns(width)
+    //Create the canvas looping through the lines(height) and columns(width)
     for(let row = 0; row < height; row++){
-      let tableRow = table.insertRow(row);
-      for(let col = 0; col < width; col++){
-        let cell = tableRow.insertCell(col);
-        cell.id = "tableData-"+row+"-"+col;
-        cell.addEventListener("click",function(){
-            cell.style.backgroundColor = color.value;
-        });
-        tableDataIds.push(cell.id);
-      }
+        let tableRow = table.insertRow(row);
+        for(let col = 0; col < width; col++){
+          let cell = tableRow.insertCell(col);
+          cell.id = "tableData-"+row+"-"+col;
+          cell.addEventListener("click",function(){
+              cell.style.backgroundColor = color.value;
+          });
+          tableDataIds.push(cell.id);
+        }
     }
 }
 
-//return the submitted values for grid dimentions
+//Return the submitted values for grid dimentions
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -47,6 +49,7 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+//Clear the canvas colored cells
 function clearCanvas(){
     for(let index = 0; index < tableDataIds.length; index++){
         document.getElementById(tableDataIds[index]).style.backgroundColor = "#ffffff";
