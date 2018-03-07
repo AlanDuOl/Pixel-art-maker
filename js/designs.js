137e09ec1a4930820827152a62dae0d7ba6afbd0
AlanDuOl <alanduarte90@gmail.com>
2018-03-06 22:39:43 -0300

    Add files via upload

--- js/designs.js
+++ js/designs.js
@@ -46,7 +46,7 @@ function getParameterByName(name, url) {
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
-    if (!results[2]) return '';
+    if (!results[2]) return "";
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
